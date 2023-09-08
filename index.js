63c4a6ea8f044b0a700115e8ecdaf5a20bc8819d
mominul <ulhouqemomin39@gmail.com>
2023-09-08 19:45:46 +0600

    added comments

--- index.js
+++ index.js
@@ -11,9 +11,11 @@ app.get("/", (req, res) => {
   res.send("Hello from the backend!");
 });
 
+// MongoDB connection URI
 const uri =
   "mongodb+srv://test-task:M4twd86A5cEEGYLu@cluster0.34btmna.mongodb.net/?retryWrites=true&w=majority";
 
+// Create a MongoDB client
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -22,29 +24,30 @@ const client = new MongoClient(uri, {
   },
 });
 
+// Define async function to run the server
 async function run() {
   try {
+    // Uncomment the following line to establish a connection to MongoDB
     // await client.connect();
+
+    // Get a reference to the "todos" collection in the "api" database
     const TodoCollection = client.db("api").collection("todos");
 
-    // Inserted Gallary section
-
+    // Get all todos
     app.get("/api/todos", async (req, res) => {
       const cursor = TodoCollection.find();
       const result = await cursor.toArray();
       res.send(result);
     });
 
- 
-    // Insert a new toy
+    // Add a new todo
     app.post("/api/todos", async (req, res) => {
-      const addedToy = req.body;
-      const result = await TodoCollection.insertOne(addedToy);
+      const addedTodo = req.body;
+      const result = await TodoCollection.insertOne(addedTodo);
       res.send(result);
     });
 
-    // updating
-
+    // Get a specific todo by ID
     app.get("/api/todos/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
@@ -52,25 +55,26 @@ async function run() {
       res.send(result);
     });
 
+    // Update a specific todo by ID
     app.put("/api/todos/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const options = { upsert: true };
-      const updateDescrition = req.body;
-      const updatedDescrition = {
+      const updateDescription = req.body;
+      const updatedDescription = {
         $set: {
-          text: updateDescrition.text,
+          text: updateDescription.text,
         },
       };
       const result = await TodoCollection.updateOne(
         query,
-        updatedDescrition,
+        updatedDescription,
         options
       );
       res.send(result);
     });
 
-    // Delete a toy by ID
+    // Delete a specific todo by ID
     app.delete("/api/todos/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
@@ -78,12 +82,11 @@ async function run() {
       res.send(result);
     });
 
+    // Ping MongoDB to check the connection
     await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensure to close the client when done
+    // Ensure to close the client when done (uncomment when needed)
     // await client.close();
   }
 }
